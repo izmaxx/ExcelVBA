f423e1bf0d4de129598b3a68f7288ed961936707
izmaxx <isunhachawi@gmail.com>
2015-11-15 23:11:07 -0500

    z3 solver example

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Z3;
+
+namespace Z3Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (Context ctx = new Context())
+            {
+                IntExpr x = ctx.MkIntConst("a");
+                IntExpr one = ctx.MkInt(3);
+                BoolExpr test = ctx.MkLt(x, one);
+                Model model = Check(ctx, test, Status.SATISFIABLE);
+            }
+
+
+        }
+
+        static Model Check(Context ctx, BoolExpr f, Status sat)
+        {
+            Solver s = ctx.MkSolver();
+            s.Assert(f);
+            if (s.Check() != sat)
+            {//do something
+            };
+            if (sat == Status.SATISFIABLE)
+            {
+                Console.WriteLine("Data" + s.Model);
+                return s.Model;
+            }
+
+            else
+                return null;
+        }
+    }
+
+}
