6b12bde257295858c69ad02fa96f78004dd4068a
izmaxx <isunhachawi@gmail.com>
2015-11-19 17:35:54 -0500

    Removing experimental files

--- ANTLRTest/Program.cs
+++ ANTLRTest/Program.cs
@@ -13,19 +13,21 @@ namespace ANTLRTest
     {
         static void Main(string[] args)
         {
+            // TODO: Receive VBA code as input from Rubberduck or an Excel workbook
             FileStream fileStream = new FileStream(@"c:\example1.txt", FileMode.Open, FileAccess.Read);
 
             Stream inputStream = Console.OpenStandardInput();
-            AntlrInputStream input = new AntlrInputStream(fileStream);
-            //AntlrInputStream input = new AntlrInputStream("{1,2}");
+            AntlrInputStream input = new AntlrInputStream(fileStream);            
             VBGrammarLexer lexer = new VBGrammarLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             VBGrammarParser parser = new VBGrammarParser(tokens);
 
-            IParseTree tree = parser.startRule(); // begin parsing at init rule
+            // Begin parsing using initial rule
+            IParseTree tree = parser.startRule(); 
             Console.WriteLine(tree.ToStringTree(parser));
 
-           Antlr4.Runtime.Misc.Utils.TE
+
+   
            // LabeledExprVisitor eval = new LabeledExprVisitor();
            // eval.Visit(tree);
 
