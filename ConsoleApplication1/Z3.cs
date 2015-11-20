5da1876dd2635b633e12007cad16ea24c705bda6
kimphan14 <kimmeh14@gmail.com>
2015-11-19 20:52:50 -0700

    Added in Z3Test to work with Z3 solver.

---
+++ ConsoleApplication1/Z3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Z3;
+
+namespace ConsoleApplication1
+{
+    class Z3
+    {
+        static void Main(string[] args)
+        {
+            using (Context ctx = new Context())
+            {
+                Expr x = ctx.MkConst("x", ctx.MkIntSort());
+                Expr zero = ctx.MkNumeral(0, ctx.MkIntSort());
+
+                Solver s = ctx.MkSolver();
+                s.Assert(ctx.MkLt((ArithExpr)zero, (ArithExpr)x)); // 0<x
+
+                Status result = s.Check();
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Z3Test/Z3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Z3;
+using Z3Solver;
+
+namespace Z3Test
+{
+    public class Z3
+    {
+        static void Main(string[] args)
+        {
+            // if 3 < x
+            Program.CheckLessThan(2, "x");
+
+            //using (Context ctx = new Context())
+            //{
+            //    Expr x = ctx.MkConst("x", ctx.MkIntSort());
+            //    Expr zero = ctx.MkNumeral(0, ctx.MkIntSort());
+
+            //    Solver s = ctx.MkSolver();
+            //    s.Assert(ctx.MkLt((ArithExpr)zero, (ArithExpr)x)); // 0<x
+
+            //    Status result = s.Check();
+            //    Console.WriteLine(result);
+            //}
+        }
+    }
+}
